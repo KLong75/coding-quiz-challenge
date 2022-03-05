99196b05e1d85b2bf01491c37d05cf5851793082
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-03-04 19:47:07 -0600

    html adjustments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
-var buttonEl = document.querySelector("#start-quiz");
+var buttonEl = document.querySelector("#start-button");
 console.log(buttonEl);
 
 buttonEl.addEventListener("click", function() {
-    alert("button clicked");
+    var quizBox = document.querySelector("#quiz-sheet");
+    var quizQuestionOne = document.createElement("div");
+    quizQuestionOne.textContent ="Which tag is used to list items with bullets?";
+    quizBox.appendChild(quizQuestionOne);
 });
+
+var runQuiz =() {
+    
+}
