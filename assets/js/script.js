b2b77581c7df0b500f2bb52b5eb87ded718310b5
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-03-04 19:56:36 -0600

    create runQuiz function, set runQuiz function to start when start button clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 var buttonEl = document.querySelector("#start-button");
 console.log(buttonEl);
 
-buttonEl.addEventListener("click", function() {
+var runQuiz = function() {
     var quizBox = document.querySelector("#quiz-sheet");
     var quizQuestionOne = document.createElement("div");
     quizQuestionOne.textContent ="Which tag is used to list items with bullets?";
     quizBox.appendChild(quizQuestionOne);
-});
-
-var runQuiz =() {
-    
 }
+
+buttonEl.addEventListener("click", runQuiz);
+
+
+
