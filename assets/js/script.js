dd3f1402a5657a5df14f3f392506b3125670525c
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-03-05 16:46:59 -0600

    logging off, saving most recent updates, nothing specific of note

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,75 @@
-var buttonEl = document.querySelector("#start-button");
 
 
 
 var runQuiz = function() {
+    
+    //  var nextQuestion = document.querySelector("#next");
+      //    document.getElementById("next-question").hidden = false;
+
     var quizBox = document.querySelector("#quiz-sheet");
-    document.getElementById("start-button-container").hidden = true;
+      document.getElementById("start-button-container").hidden = true;
+
+
     var quizQuestionOne = document.createElement("div");
-    quizQuestionOne.className ="quiz-question";
-    quizQuestionOne.textContent ="1. What does HTML stand for?";
-    quizBox.appendChild(quizQuestionOne);
+      quizQuestionOne.className ="quiz-question";
+      quizQuestionOne.textContent ="1. What does HTML stand for?";
+      quizBox.appendChild(quizQuestionOne);
 
     var answerChoiceBoxA = document.createElement("div");
-    answerChoiceBoxA.className = "container";
-    //answerChoiceBoxA.id = "incorrect";
-   // answerChoiceBoxA.onclick(checkAnswer('A'));
-    quizBox.appendChild(answerChoiceBoxA); 
-
-
-
-
-
+      answerChoiceBoxA.className = "container";
+      quizBox.appendChild(answerChoiceBoxA); 
 
     var answerChoiceA = document.createElement("button");
-    answerChoiceA.id = "incorrect";
-    answerChoiceA.className = "container text-left btn btn-secondary";
-    answerChoiceA.textContent="A. Hyper Toddler Makes Lasagna";
-    answerChoiceBoxA.appendChild(answerChoiceA);
-    document.getElementById("incorrect").addEventListener("click", checkAnswerA);
-    function checkAnswerA() {
+      answerChoiceA.id = "incorrect";
+      answerChoiceA.className = "container text-left btn btn-secondary";
+      answerChoiceA.textContent="A. Hyper Toddler Makes Lasagna";
+      answerChoiceBoxA.appendChild(answerChoiceA);
+      document.getElementById("incorrect").addEventListener("click", checkAnswerA);
+      function checkAnswerA() {
         document.getElementById("incorrect").innerHTML = "Incorrect";
     }
 
-
-
-
-
-
-    
-
     var answerChoiceBoxB = document.createElement("div");
-    answerChoiceBoxB.className = "container";
-   // answerChoiceBoxB.id = "correct";
-   // answerChoiceBoxB.onclick(checkAnswer('B'));
-    quizBox.appendChild(answerChoiceBoxB); 
+      answerChoiceBoxB.className = "container";
+      quizBox.appendChild(answerChoiceBoxB); 
 
     var answerChoiceB = document.createElement("button");
-    answerChoiceB.id = "correct"
-    answerChoiceB.className = "container text-left btn btn-secondary"
-    answerChoiceB.textContent="B. Hypertext Markup Language";
-    answerChoiceBoxB.appendChild(answerChoiceB);
-    document.getElementById("correct").addEventListener("click", checkAnswerB);
-    function checkAnswerB() {
-        document.getElementById("correct").innerHTML = "Correct";
-    }
+      answerChoiceB.id = "correct"
+      answerChoiceB.className = "container text-left btn btn-secondary"
+      answerChoiceB.textContent="B. Hypertext Markup Language";
+      answerChoiceBoxB.appendChild(answerChoiceB);
+      document.getElementById("correct").addEventListener("click", checkAnswerB);
+        function checkAnswerB() {
+          document.getElementById("correct").innerHTML = "Correct";
+        }
    
 
     var answerChoiceBoxC = document.createElement("div");
-    answerChoiceBoxC.className = "container";
-   // answerChoiceBoxC.id = "incorrect";
-    quizBox.appendChild(answerChoiceBoxC); 
+      answerChoiceBoxC.className = "container";
+      quizBox.appendChild(answerChoiceBoxC); 
     
 
     var answerChoiceC = document.createElement("button");
-    answerChoiceC.id = "incorrect-c";
-    answerChoiceC.className = "container text-left btn btn-secondary";
-    answerChoiceC.textContent="C. How To Make Lasagna";
-    answerChoiceBoxC.appendChild(answerChoiceC);
-    //buttonEl.addEventListener("click", console.log('incorrect'));
-    document.getElementById("incorrect-c").addEventListener("click", checkAnswerC);
-    function checkAnswerC() {
+      answerChoiceC.id = "incorrect-c";
+      answerChoiceC.className = "container text-left btn btn-secondary";
+      answerChoiceC.textContent="C. How To Make Lasagna";
+      answerChoiceBoxC.appendChild(answerChoiceC);
+      document.getElementById("incorrect-c").addEventListener("click", checkAnswerC);
+      function checkAnswerC() {
         document.getElementById("incorrect-c").innerHTML = "Incorrect";
+      }
+      
+
+    /*var nextQuestion = document.createElement("button")
+      nextQuestion.id = "next-question";
+      nextQuestion.className = "next-question btn btn-success";
+      nextQuestion.textContent = "Next Question"
+      quizBox.appendChild(nextQuestion);*/
     }
-
-
+    
+    
+    document.getElementById("start-button").addEventListener("click", runQuiz);
+    
    // var answerChoiceA = document.createElement("button");
   //  answerChoiceA.textContent="A.";
    // quizBox.appendChild(answerChoiceA);
@@ -94,9 +91,6 @@ var runQuiz = function() {
     testAnswerChoice.className = "container text-left"
     testAnswerChoice.textContent="A. rgrgrgasgasg";
     answerTest.appendChild(testAnswerChoice);*/
-}
-
-document.getElementById("start-button").addEventListener("click", runQuiz);
 
 
 
