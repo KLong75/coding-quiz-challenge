0ab2acd16bd7e9037f1c5bb9448a926fec7c9f4a
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-03-05 11:36:40 -0600

    create answer choice buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,56 @@ var runQuiz = function() {
     var quizQuestionOne = document.createElement("div");
     quizQuestionOne.textContent ="Which tag is used to list items with bullets?";
     quizBox.appendChild(quizQuestionOne);
+
+    var answerChoiceBoxA = document.createElement("div");
+    answerChoiceBoxA.className = "container";
+    quizBox.appendChild(answerChoiceBoxA); 
+
+    var answerChoiceA = document.createElement("button");
+    answerChoiceA.className = "container text-left"
+    answerChoiceA.textContent="A. rgrgrgasgasg";
+    answerChoiceBoxA.appendChild(answerChoiceA);
+
+    var answerChoiceBoxB = document.createElement("div");
+    answerChoiceBoxB.className = "container";
+    quizBox.appendChild(answerChoiceBoxB); 
+
+    var answerChoiceB = document.createElement("button");
+    answerChoiceB.className = "container text-left"
+    answerChoiceB.textContent="B. rgrgrgasgasg";
+    answerChoiceBoxB.appendChild(answerChoiceB);
+
+    var answerChoiceBoxC = document.createElement("div");
+    answerChoiceBoxC.className = "container";
+    quizBox.appendChild(answerChoiceBoxC); 
+
+    var answerChoiceC = document.createElement("button");
+    answerChoiceC.className = "container text-left"
+    answerChoiceC.textContent="C. rgrgrgasgasg";
+    answerChoiceBoxC.appendChild(answerChoiceC);
+
+
+   // var answerChoiceA = document.createElement("button");
+  //  answerChoiceA.textContent="A.";
+   // quizBox.appendChild(answerChoiceA);
+
+   // var answerChoiceB = document.createElement("button");
+    //answerChoiceB.textContent="B.";
+    //quizBox.appendChild(answerChoiceB);
+
+    //var answerChoiceC = document.createElement("button");
+    //answerChoiceC.textContent="C.";
+    //quizBox.appendChild(answerChoiceC);
+
+   /* var answerTest = document.createElement("div");
+    answerTest.className = "container";
+    //answerTest.textContent="Afadfasdfadgfasdgasdg.";
+    quizBox.appendChild(answerTest);
+    
+    var testAnswerChoice = document.createElement("button");
+    testAnswerChoice.className = "container text-left"
+    testAnswerChoice.textContent="A. rgrgrgasgasg";
+    answerTest.appendChild(testAnswerChoice);*/
 }
 
 buttonEl.addEventListener("click", runQuiz);
