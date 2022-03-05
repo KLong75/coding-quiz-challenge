66d4d3fb2769e62b5fa3ed0bde45b305b0e893f9
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-03-04 23:15:09 -0600

    adding html for quiz question and answer choices

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 var buttonEl = document.querySelector("#start-button");
-console.log(buttonEl);
+
+
 
 var runQuiz = function() {
     var quizBox = document.querySelector("#quiz-sheet");
+    document.getElementById("start-button-container").hidden = true;
     var quizQuestionOne = document.createElement("div");
     quizQuestionOne.textContent ="Which tag is used to list items with bullets?";
     quizBox.appendChild(quizQuestionOne);
