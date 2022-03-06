b80a3b4d561a32f180aa91511083c79db70f43f9
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-03-06 17:35:48 -0600

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
 
 //var buttonEl = document.querySelector("#next-question");
+var timeleft = 100;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Finished";
+  } else {
+    document.getElementById("countdown").innerHTML = "Timer: " + timeleft + " seconds remaining";
+  }
+  timeleft -= 1;
+}, 1000);
+
+
 
 var runQuiz = function() {
     
